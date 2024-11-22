7fcf70f001daaf85f24ed6bd686c521c27825fa5
angelchen7 <angelchen7777@yahoo.com>
2024-11-22 10:28:33 -0800

    feat: namespaced every `sf` and `tidyverse` function

--- 01_Flow_FireData_Harmonization_Angel.R
+++ 01_Flow_FireData_Harmonization_Angel.R
@@ -54,21 +54,21 @@ for (i in 1:length(raw_files_EVER)){
   # If the file name is "EVER_FIRES_2020.shp" or "EVER_Fires_2021.shp"...
   if (raw_file_name == "EVER_FIRES_2020.shp" | raw_file_name == "EVER_Fires_2021.shp"){
     # Read in shapefile
-    shp <- read_sf(file.path(folder_EVER, raw_files_EVER[i])) %>% 
+    shp <- sf::read_sf(file.path(folder_EVER, raw_files_EVER[i])) %>% 
       # Drop Z dimension
-      st_zm(shp, drop=T, what = "ZM") %>%
+      sf::st_zm(shp, drop=T, what = "ZM") %>%
       # Transform to WGS84
-      st_transform("EPSG:4326") %>% 
+      sf::st_transform("EPSG:4326") %>% 
       # Make valid
-      st_make_valid()
+      sf::st_make_valid()
     
   } else {
   # Read in shapefile
-  shp <- read_sf(file.path(folder_EVER, raw_files_EVER[i])) %>% 
+  shp <- sf::read_sf(file.path(folder_EVER, raw_files_EVER[i])) %>% 
     # Transform to WGS84
-    st_transform("EPSG:4326") %>% 
+    sf::st_transform("EPSG:4326") %>% 
     # Make valid
-    st_make_valid()
+    sf::st_make_valid()
   }
   
   # Add to list
@@ -90,54 +90,54 @@ for (i in 1:length(shp_list_EVER)){
   if (file_name %in% years_1948_2017){
     shp_list_EVER[[file_name]] <- shp_list_EVER[[file_name]] %>%
       # Select relevant columns
-      select(FIRE_ID, FIRE_NUM, FIRE_NAME, YEAR_, DISC_DATE, DECLD_DATE, FIRE_TYPE) %>%
+      dplyr::select(FIRE_ID, FIRE_NUM, FIRE_NAME, YEAR_, DISC_DATE, DECLD_DATE, FIRE_TYPE) %>%
       # Standardize column names
-      rename(Fire_ID = FIRE_ID) %>%
-      rename(Fire_Number = FIRE_NUM) %>%
-      rename(Fire_Name = FIRE_NAME) %>%
-      rename(Year = YEAR_) %>%
-      rename(Disc_Date = DISC_DATE) %>%
-      rename(Decld_Date = DECLD_DATE) %>%
-      rename(Fire_Type = FIRE_TYPE)  %>%
+      dplyr::rename(Fire_ID = FIRE_ID) %>%
+      dplyr::rename(Fire_Number = FIRE_NUM) %>%
+      dplyr::rename(Fire_Name = FIRE_NAME) %>%
+      dplyr::rename(Year = YEAR_) %>%
+      dplyr::rename(Disc_Date = DISC_DATE) %>%
+      dplyr::rename(Decld_Date = DECLD_DATE) %>%
+      dplyr::rename(Fire_Type = FIRE_TYPE)  %>%
       # Create a new column to denote the shapefile where the data came from
-      mutate(File_Name = file_name, .before = Fire_ID) %>%
+      dplyr::mutate(File_Name = file_name, .before = Fire_ID) %>%
       # Make all columns into character columns
-      mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
+      dplyr::mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
   }
   
   # If the file is from 2018 or 2019...
   if (file_name == "EVER_FIRES_2018.shp" | file_name == "EVER_FIRES_2019.shp"){
     shp_list_EVER[[file_name]] <- shp_list_EVER[[file_name]] %>%
       # Select relevant columns
-      select(UniqueFire, LocalIncid, FIRE_NAME, YEAR, DISC_DATE, FireOutDat, FIRE_TYPE) %>%
+      dplyr::select(UniqueFire, LocalIncid, FIRE_NAME, YEAR, DISC_DATE, FireOutDat, FIRE_TYPE) %>%
       # Standardize column names
-      rename(Fire_ID = UniqueFire) %>%
-      rename(Fire_Number = LocalIncid) %>%
-      rename(Fire_Name = FIRE_NAME) %>%
-      rename(Year = YEAR) %>%
-      rename(Disc_Date = DISC_DATE) %>%
-      rename(Decld_Date = FireOutDat) %>%
-      rename(Fire_Type = FIRE_TYPE) %>%
+      dplyr::rename(Fire_ID = UniqueFire) %>%
+      dplyr::rename(Fire_Number = LocalIncid) %>%
+      dplyr::rename(Fire_Name = FIRE_NAME) %>%
+      dplyr::rename(Year = YEAR) %>%
+      dplyr::rename(Disc_Date = DISC_DATE) %>%
+      dplyr::rename(Decld_Date = FireOutDat) %>%
+      dplyr::rename(Fire_Type = FIRE_TYPE) %>%
       # Create a new column to denote the shapefile where the data came from
-      mutate(File_Name = file_name, .before = Fire_ID) %>%
+      dplyr::mutate(File_Name = file_name, .before = Fire_ID) %>%
       # Make all columns into character columns
-      mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
+      dplyr::mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
   }
   
   # If the file is from 2020 or 2021...
   if (file_name == "EVER_FIRES_2020.shp" | file_name == "EVER_Fires_2021.shp") {
     shp_list_EVER[[file_name]] <- shp_list_EVER[[file_name]] %>%
       # Select relevant columns
-      select(FIRE_ID, FIRE_NUM, FIRE_NAME, CY_YEAR, DISC_DATE, DECLD_DATE, Incident_t, fire_cause) %>%
+      dplyr::select(FIRE_ID, FIRE_NUM, FIRE_NAME, CY_YEAR, DISC_DATE, DECLD_DATE, Incident_t, fire_cause) %>%
       # Standardize column names
-      rename(Fire_ID = FIRE_ID) %>%
-      rename(Fire_Number = FIRE_NUM) %>%
-      rename(Fire_Name = FIRE_NAME) %>%
-      rename(Year = CY_YEAR) %>%
-      rename(Disc_Date = DISC_DATE) %>%
-      rename(Decld_Date = DECLD_DATE) %>%
+      dplyr::rename(Fire_ID = FIRE_ID) %>%
+      dplyr::rename(Fire_Number = FIRE_NUM) %>%
+      dplyr::rename(Fire_Name = FIRE_NAME) %>%
+      dplyr::rename(Year = CY_YEAR) %>%
+      dplyr::rename(Disc_Date = DISC_DATE) %>%
+      dplyr::rename(Decld_Date = DECLD_DATE) %>%
       # Make a new Fire_Type column according to these conditions:
-      mutate(Fire_Type = case_when(
+      dplyr::mutate(Fire_Type = dplyr::case_when(
         # When Incident_t is "WF" or "FU", set the value to "11"
         Incident_t == "WF" | Incident_t == "FU" ~ "11",
         # When Incident_t is "RX" and fire_cause is "Management", set the value to "48"
@@ -146,11 +146,11 @@ for (i in 1:length(shp_list_EVER)){
         Incident_t == "RX" & fire_cause != "Management" ~ "11",
       )) %>%
       # Drop Incident_t and fire_cause columns
-      select(-Incident_t, -fire_cause) %>%
+      dplyr::select(-Incident_t, -fire_cause) %>%
       # Create a new column to denote the shapefile where the data came from
-      mutate(File_Name = file_name, .before = Fire_ID) %>%
+      dplyr::mutate(File_Name = file_name, .before = Fire_ID) %>%
       # Make all columns into character columns
-      mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
+      dplyr::mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
   }
 }
 
@@ -161,7 +161,7 @@ tidy_v0_EVER <- shp_list_EVER %>%
 # RESOLVING REMAINING DATE FORMAT ISSUE -------------
 
 tidy_v1_EVER <- tidy_v0_EVER %>%
-  mutate(Decld_Date = case_when(
+  dplyr::mutate(Decld_Date = dplyr::case_when(
     # Fix wrong dates in Decld_Date column
     # For example, in the 1955 shapefile, change the Decld_Date from "16550404" to "19550404"
     File_Name == "EVER_FIRES_1955.shp" & Decld_Date == "16550404" ~ "19550404",
@@ -181,13 +181,13 @@ tidy_v1_EVER <- tidy_v0_EVER %>%
     Decld_Date == "0" ~ NA,
     T ~ Decld_Date
   )) %>%
-  mutate(Year = case_when(
+  dplyr::mutate(Year = dplyr::case_when(
     # Fix wrong years in the Year column
     File_Name == "EVER_FIRES_2017.shp" & Year == "0" ~ "2017",
     File_Name == "EVER_FIRES_2018.shp" & Year == "2017" ~ "2018",
     T ~ Year
   )) %>%
-  mutate(Disc_Date = case_when(
+  dplyr::mutate(Disc_Date = dplyr::case_when(
     # Fix wrong dates in Disc_Date column
     File_Name == "EVER_FIRES_2020.shp" & Disc_Date == "2020324" ~ "20200324",
     File_Name == "EVER_FIRES_2020.shp" & Disc_Date == "2020704" ~ "20200704",
@@ -195,7 +195,7 @@ tidy_v1_EVER <- tidy_v0_EVER %>%
     Disc_Date == "0" ~ NA,
     T ~ Disc_Date
   )) %>%
-  mutate(Fire_Type = case_when(
+  dplyr::mutate(Fire_Type = dplyr::case_when(
     # If Fire_Type is 48, change it to RX,
     # Otherwise if it's not NA, change it to WF
     Fire_Type == "48" ~ "RX",
@@ -206,7 +206,7 @@ tidy_v1_EVER <- tidy_v0_EVER %>%
 # The 2016-2019 shapefiles have different date formats in their Disc_Date and Decld_Date columns
 # These need to be changed to YYYYMMDD like the rest of the files
 tidy_v2_EVER <- tidy_v1_EVER %>%
-  mutate(date_format = case_when(
+  dplyr::mutate(date_format = dplyr::case_when(
     # Create a new date_format column that denotes each file's date formats
     File_Name == "EVER_FIRES_2016.shp" ~ "MMDDYYYY",
     File_Name == "EVER_FIRES_2017.shp" & stringr::str_detect(Disc_Date, "2017$") ~ "MMDDYYYY",
@@ -214,64 +214,64 @@ tidy_v2_EVER <- tidy_v1_EVER %>%
     File_Name == "EVER_FIRES_2018.shp" | File_Name == "EVER_FIRES_2019.shp" ~ "YYYY/MM/DD",
     T ~ "YYYYMMDD"
   )) %>%
-  mutate(year_fix_disc = case_when(
+  dplyr::mutate(year_fix_disc = dplyr::case_when(
     # Create a new year_fix_disc column that contains the year from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" ~ stringr::str_extract(Disc_Date, "[:digit:]{4}$"),
   )) %>%
-  mutate(year_fix_decld = case_when(
+  dplyr::mutate(year_fix_decld = dplyr::case_when(
     # Create a new year_fix_decld column that contains the year from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" ~ stringr::str_extract(Decld_Date, "[:digit:]{4}$"),
   )) %>%
-  mutate(month_fix_disc = case_when(
+  dplyr::mutate(month_fix_disc = dplyr::case_when(
     # Create a new month_fix_disc column that contains the month from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" & nchar(Disc_Date) == 7 ~ paste0("0", stringr::str_extract(Disc_Date, "^[:digit:]{1}")),
     date_format == "MMDDYYYY" & nchar(Disc_Date) == 8 ~ stringr::str_extract(Disc_Date, "^[:digit:]{2}"),
   )) %>%
-  mutate(month_fix_decld = case_when(
+  dplyr::mutate(month_fix_decld = dplyr::case_when(
     # Create a new month_fix_decld column that contains the month from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" & nchar(Decld_Date) == 7 ~ paste0("0", stringr::str_extract(Decld_Date, "^[:digit:]{1}")),
     date_format == "MMDDYYYY" & nchar(Decld_Date) == 8 ~ stringr::str_extract(Decld_Date, "^[:digit:]{2}"),
   )) %>%
-  mutate(day_fix_disc = case_when(
+  dplyr::mutate(day_fix_disc = dplyr::case_when(
     # Create a new day_fix_disc column that contains the MMDD from rows that have MMDDYYYY
     date_format == "MMDDYYYY" ~ stringr::str_replace(Disc_Date, "[:digit:]{4}$", ""),
   )) %>%
-  mutate(day_fix_disc = case_when(
+  dplyr::mutate(day_fix_disc = dplyr::case_when(
     # Edit the day_fix_disc column to contain the day from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" ~ stringr::str_extract(day_fix_disc, "[:digit:]{2}$"),
   )) %>%
-  mutate(day_fix_decld = case_when(
+  dplyr::mutate(day_fix_decld = dplyr::case_when(
     # Create a new day_fix_decld column that contains the MMDD from rows that have MMDDYYYY
     date_format == "MMDDYYYY" ~ stringr::str_replace(Decld_Date, "[:digit:]{4}$", ""),
   )) %>%
-  mutate(day_fix_decld = case_when(
+  dplyr::mutate(day_fix_decld = dplyr::case_when(
     # Edit the day_fix_decld column to contain the day from rows that have MMDDYYYY format
     date_format == "MMDDYYYY" ~ stringr::str_extract(day_fix_decld, "[:digit:]{2}$"),
   )) %>%
-  mutate(Disc_Date_fix = case_when(
+  dplyr::mutate(Disc_Date_fix = dplyr::case_when(
     # Create a Disc_Date_fix that contains the fixed discovered date in YYYYMMDD format 
     date_format == "MMDDYYYY" ~ paste0(year_fix_disc, month_fix_disc, day_fix_disc),
     date_format == "YYYY/MM/DD" ~ stringr::str_replace_all(Disc_Date, "/", ""),
     T ~ Disc_Date
   ), .after = Disc_Date) %>%
-  mutate(Decld_Date_fix = case_when(
+  dplyr::mutate(Decld_Date_fix = dplyr::case_when(
     # Create a Decld_Date_fix that contains the fixed declined date in YYYYMMDD format 
     date_format == "MMDDYYYY" ~ paste0(year_fix_decld, month_fix_decld, day_fix_decld),
     date_format == "YYYY/MM/DD" ~ stringr::str_replace_all(Decld_Date, "/", ""),
     T ~ Decld_Date
   ), .after = Decld_Date) %>%
   # Drop unneeded columns
-  select(-Disc_Date, -Decld_Date, -date_format,
+  dplyr::select(-Disc_Date, -Decld_Date, -date_format,
          -year_fix_disc, -month_fix_disc, -day_fix_disc,
          -year_fix_decld, -month_fix_decld, -day_fix_decld) %>%
   # Rename the fixed columns 
-  rename(Disc_Date = Disc_Date_fix) %>%
-  rename(Decld_Date = Decld_Date_fix) %>%
+  dplyr::rename(Disc_Date = Disc_Date_fix) %>%
+  dplyr::rename(Decld_Date = Decld_Date_fix) %>%
   # Convert all dates into proper Date columns
-  mutate(Disc_Date = readr::parse_date(Disc_Date, "%Y%m%d"),
+  dplyr::mutate(Disc_Date = readr::parse_date(Disc_Date, "%Y%m%d"),
          Decld_Date = readr::parse_date(Decld_Date, "%Y%m%d")) %>%
   # Convert Year to a numeric column
-  mutate(Year = as.numeric(Year))
+  dplyr::mutate(Year = as.numeric(Year))
 
 ## ----------------------------------------------- ##
 # FIXING ISSUES WITH BIG CYPRESS FIRE DATA (BICY) ----
@@ -285,27 +285,27 @@ folder_BICY <- "BICY_FIRE_Perimeter_original"
 ( raw_file_BICY <- dir(path = folder_BICY, "*.shp$") ) 
 
 # Read in shapefile
-tidy_v0_BICY <- read_sf(file.path(folder_BICY, raw_file_BICY)) %>%
+tidy_v0_BICY <- sf::read_sf(file.path(folder_BICY, raw_file_BICY)) %>%
   # Transform to WGS84
-  st_transform("EPSG:4326") %>%
+  sf::st_transform("EPSG:4326") %>%
   # Make valid
-  st_make_valid()
+  sf::st_make_valid()
 
 # FORMAT COLUMNS TO MATCH EVERGLADES -------------
 
 tidy_v1_BICY <- tidy_v0_BICY %>%
   # Select relevant columns
-  select(FIRE_ID, FireNumber, FireName, CalendYear, StartDate, EndDate, FireType) %>%
+  dplyr::select(FIRE_ID, FireNumber, FireName, CalendYear, StartDate, EndDate, FireType) %>%
   # Standardize column names
-  rename(Fire_ID = FIRE_ID) %>%
-  rename(Fire_Number = FireNumber) %>%
-  rename(Fire_Name = FireName) %>%
-  rename(Year = CalendYear) %>%
-  rename(Disc_Date = StartDate) %>%
-  rename(Decld_Date = EndDate) %>%
-  rename(Fire_Type = FireType) %>%
+  dplyr::rename(Fire_ID = FIRE_ID) %>%
+  dplyr::rename(Fire_Number = FireNumber) %>%
+  dplyr::rename(Fire_Name = FireName) %>%
+  dplyr::rename(Year = CalendYear) %>%
+  dplyr::rename(Disc_Date = StartDate) %>%
+  dplyr::rename(Decld_Date = EndDate) %>%
+  dplyr::rename(Fire_Type = FireType) %>%
   # Edit Fire_Type column according to these conditions:
-  mutate(Fire_Type = case_when(
+  dplyr::mutate(Fire_Type = dplyr::case_when(
     # When Fire_Type is "Wildland", "Other", "Unknown", "Vehicle Fire", "Fire Use", or "Chemical", set the value to "WF"
     Fire_Type %in% c("Wildland", "Other", "Unknown",
                      "Vehicle Fire", "Fire Use", "Chemical") ~ "WF",
@@ -313,21 +313,21 @@ tidy_v1_BICY <- tidy_v0_BICY %>%
     Fire_Type %in% c("Prescribed", "Burn Area", "Mechanical") ~ "RX"
   )) %>%
   # Create a new column to denote the shapefile where the data came from
-  mutate(File_Name = raw_file_BICY, .before = Fire_ID) %>%
+  dplyr::mutate(File_Name = raw_file_BICY, .before = Fire_ID) %>%
   # Make all columns into character columns
-  mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
+  dplyr::mutate(dplyr::across(.cols = -c(geometry), .fns = as.character))
   
 # RESOLVING REMAINING DATE FORMAT ISSUE -------------
 
 tidy_v2_BICY <- tidy_v1_BICY %>%
   # Create a flag for missing start/end days
-  mutate(Date_Flag = case_when(
+  dplyr::mutate(Date_Flag = dplyr::case_when(
     Disc_Date %in% c("2003/09/", "2003/07/", "2007/05",
                      "2014/05/xx", "2014/06/xx") ~ "Missing exact start day, so it has been set to the last day of the month",
     Decld_Date %in% c("2016/06/xx", "2016/10/xx") ~ "Missing exact end day, so it has been set to the last day of the month",
     T ~ NA
   ), .after = Decld_Date) %>%
-  mutate(Disc_Date = case_when(
+  dplyr::mutate(Disc_Date = dplyr::case_when(
     # Fix wrong dates in Disc_Date column
     Disc_Date == "1981/0208" ~ "1981/02/08",
     Disc_Date == "198/103/29" ~ "1981/03/29",
@@ -343,7 +343,7 @@ tidy_v2_BICY <- tidy_v1_BICY %>%
     Disc_Date == "2014/06/xx" ~ "2014/06/30",
     T ~ Disc_Date
   )) %>%
-  mutate(Decld_Date = case_when(
+  dplyr::mutate(Decld_Date = dplyr::case_when(
     # Fix wrong dates in Decld_Date column
     Decld_Date == "7/16/2020" ~ "2020/07/16",
     Decld_Date == "07/14/2020" ~ "2020/07/14",
@@ -359,10 +359,10 @@ tidy_v2_BICY <- tidy_v1_BICY %>%
     T ~ Decld_Date
   )) %>%
   # Convert all dates into proper Date columns
-  mutate(Disc_Date = readr::parse_date(Disc_Date, "%Y/%m/%d"),
+  dplyr::mutate(Disc_Date = readr::parse_date(Disc_Date, "%Y/%m/%d"),
          Decld_Date = readr::parse_date(Decld_Date, "%Y/%m/%d")) %>%
   # Convert Year to a numeric column
-  mutate(Year = as.numeric(Year))
+  dplyr::mutate(Year = as.numeric(Year))
 
 
 ## ------------------------------------------------------------ ##
@@ -371,12 +371,12 @@ tidy_v2_BICY <- tidy_v1_BICY %>%
 
 # Clip EVER fire dataset down to same timeframe as BICY (1978-2021)
 tidy_v3_EVER <- tidy_v2_EVER %>%
-  filter(Year >= 1978)
+  dplyr::filter(Year >= 1978)
 
 # Combine EVER and BICY shapefiles together
 tidy_v0_EVER_BICY <- tidy_v3_EVER %>%
-  bind_rows(tidy_v2_BICY) %>%
-  relocate(Date_Flag, .after = Decld_Date)
+  dplyr::bind_rows(tidy_v2_BICY) %>%
+  dplyr::relocate(Date_Flag, .after = Decld_Date)
 
 # Export harmonized tidy fire perimeters
-st_write(tidy_v0_EVER_BICY, file.path("tidy_perimeters", "EVER_BICY_1978_2021_perim.shp"))
+sf::st_write(tidy_v0_EVER_BICY, file.path("tidy_perimeters", "EVER_BICY_1978_2021_perim.shp"))
