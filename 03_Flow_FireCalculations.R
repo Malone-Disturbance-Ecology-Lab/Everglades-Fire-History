382c037dc9daf9cf6398238f1b0df7d046541131
Angel Chen <angelchen7777@yahoo.com>
2025-01-15 15:47:45 -0800

    refactor: replaced `num_years` to `end_year` in `total_fires()`

--- 03_Flow_FireCalculations.R
+++ 03_Flow_FireCalculations.R
@@ -58,20 +58,20 @@ names(burned_rasters_reordered)
 # Calculating total fires (1978-2023) ------------------------------------------------
 
 # Testing
-# x <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2010, num_years = 1)
+# x <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2010, end_year = 2011)
 # terra::plot(x)
 # 
-# y <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2014, num_years = 3)
+# y <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2014, end_year = 2017)
 # terra::plot(y, ext = c(400000, 600000, 2770000, 2940000))
 # terra::plot(y, ext = c(485000, 500000, 2870000, 2885000),
 #             main = "Total fires: how many times an area got burned from 2014 to 2017")
 # 
-# z <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2012, num_years = 10)
+# z <- total_fires(raster_stack = burned_rasters_reordered, start_year = 2012, end_year = 2022)
 # terra::plot(z, ext = c(485000, 520000, 2855000, 2885000),
 #             main = "Total fires: how many times an area got burned from 2012 to 2022")
 
 # Find the total fires from 1978 to 2023
-total_fires_1978_2023 <- total_fires(raster_stack = burned_rasters_reordered, start_year = 1978, num_years = 45)
+total_fires_1978_2023 <- total_fires(raster_stack = burned_rasters_reordered, start_year = 1978, end_year = 2023)
 # Check plot
 terra::plot(total_fires_1978_2023, ext = c(460000, 540000, 2815000, 2900000),
             main = "Total fires: how many times an area got burned from 1978 to 2023")
--- R/my_functions.R
+++ R/my_functions.R
@@ -1,17 +1,17 @@
 # Function for summing up 0/1 fire rasters from a raster stack
 # raster_stack: SpatRaster 
 # start_year: (numeric) the starting year
-# num_years: (numeric) number of years from the starting year (specifies the endpoint for the time interval)
-total_fires <- function(raster_stack, start_year, num_years){
+# end_year: (numeric) the ending year
+total_fires <- function(raster_stack, start_year, end_year){
   
   # Error out if the stack of rasters is not a SpatRaster
   if (class(raster_stack) != "SpatRaster"){
     stop("The stack of rasters needs to be in SpatRaster format")
   }
   
-  # Error out if the specified start year or time interval is not numeric
-  if (!is.numeric(start_year) | !is.numeric(num_years)) {
-    stop("Start year or time interval is not numeric")
+  # Error out if the specified start or end year is not numeric
+  if (!is.numeric(start_year) | !is.numeric(end_year)) {
+    stop("Start or end year is not numeric")
   }
   
   # Assemble the name of the layer for the specified starting year
@@ -20,8 +20,14 @@ total_fires <- function(raster_stack, start_year, num_years){
   # Find the numeric index of the layer that has the starting year
   start_index <- which(names(raster_stack) == start_layer_name)
   
+  # Assemble the name of the layer for the specified ending year
+  end_layer_name <- paste0("burned_", end_year)
+  
+  # Find the numeric index of the layer that has the ending year
+  end_index <- which(names(raster_stack) == end_layer_name)
+  
   # Subset the raster stack to the specified time interval
-  subset_raster_stack <- terra::subset(raster_stack, start_index:(start_index+num_years))
+  subset_raster_stack <- terra::subset(raster_stack, start_index:end_index)
   
   # Sum the layers in the subset
   # If na.rm = FALSE then only the overlapping areas will show up
